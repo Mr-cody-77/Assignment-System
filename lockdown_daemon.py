68a1a19f4ab47340a60a3643453d312b8edaa537
Mr-cody-77 <sonu77mahata@gmail.com>
2026-08-02 15:05:45 +0530

    Network Updation

--- lockdown_daemon.py
+++ lockdown_daemon.py
@@ -147,7 +147,7 @@ def lock_internet():
             "Remove-NetFirewallRule -DisplayName 'ExamSystem_BlockInternet' -ErrorAction SilentlyContinue; "
             "Remove-NetFirewallRule -DisplayName 'ExamSystem_BlockAll' -ErrorAction SilentlyContinue; "
             "Remove-NetFirewallRule -DisplayName 'ExamSystem_AllowLAN' -ErrorAction SilentlyContinue; "
-            "New-NetFirewallRule -DisplayName 'ExamSystem_BlockInternet' -Direction Outbound -Action Block -RemoteAddress '0.0.0.0-9.255.255.255', '11.0.0.0-126.255.255.255', '128.0.0.0-172.15.255.255', '172.32.0.0-192.167.255.255', '192.169.0.0-255.255.255.255' -Enabled True;"
+            "New-NetFirewallRule -DisplayName 'ExamSystem_BlockInternet' -Direction Outbound -Action Block -RemoteAddress '0.0.0.0-9.255.255.255', '11.0.0.0-126.255.255.255', '128.0.0.0-172.15.255.255', '172.32.0.0-192.167.255.255', '192.169.0.0-223.255.255.255', '240.0.0.0-255.255.255.254' -Enabled True;"
         )
         res1 = subprocess.run([
             'powershell', '-Command', script
