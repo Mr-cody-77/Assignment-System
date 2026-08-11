a737798be38f2366d1b010b1e3882c41b07741c0
Mr-cody-77 <sonu77mahata@gmail.com>
2026-08-11 14:15:18 +0530

    codeeditor bug fix-2

--- Frontend/system_interface/src/components/CodeEditor/CodeEditor.jsx
+++ Frontend/system_interface/src/components/CodeEditor/CodeEditor.jsx
@@ -26,18 +26,18 @@ const TEMPLATES = {
 window.MonacoEnvironment = {
   getWorker: function (_moduleId, label) {
     if (label === 'json') {
-      return new Worker(new URL('monaco-editor/esm/vs/language/json/json.worker', import.meta.url), { type: 'module' });
+      return new Worker(new URL('monaco-editor/language/json/json.worker', import.meta.url), { type: 'module' });
     }
     if (label === 'css' || label === 'scss' || label === 'less') {
-      return new Worker(new URL('monaco-editor/esm/vs/language/css/css.worker', import.meta.url), { type: 'module' });
+      return new Worker(new URL('monaco-editor/language/css/css.worker', import.meta.url), { type: 'module' });
     }
     if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new Worker(new URL('monaco-editor/esm/vs/language/html/html.worker', import.meta.url), { type: 'module' });
+      return new Worker(new URL('monaco-editor/language/html/html.worker', import.meta.url), { type: 'module' });
     }
     if (label === 'typescript' || label === 'javascript') {
-      return new Worker(new URL('monaco-editor/esm/vs/language/typescript/ts.worker', import.meta.url), { type: 'module' });
+      return new Worker(new URL('monaco-editor/language/typescript/ts.worker', import.meta.url), { type: 'module' });
     }
-    return new Worker(new URL('monaco-editor/esm/vs/editor/editor.worker', import.meta.url), { type: 'module' });
+    return new Worker(new URL('monaco-editor/editor/editor.worker', import.meta.url), { type: 'module' });
   }
 };
 
